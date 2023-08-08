ad600e823959b84b1b1352a3635f9cae72da736a
Andres Henriquez <rymbell@gmail.com>
2023-08-08 15:18:42 +0900

    advences

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # list all
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # create one
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # list one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # update one
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete one
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 
   # Defines the root path route ("/")
   # root "articles#index"
